966e5d590a21eff9a8c4b7f5d03ef36751d28cd0
juanma-dev <juanmanuelcastellanoshernandez@gmail.com>
2024-09-21 15:23:35 -0500

    Cuba BCI Version 4.0.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ function Header() {
             <meta charset="UTF-8" />
             <link rel="icon" type="image/x-icon" href="/favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Connection to the brain in Cuba to control the people. Conexión al cerebro en Cuba para controlar al pueblo."></meta>
             <title>Denuncia Cuba BCI</title>
         </Head>
     );
